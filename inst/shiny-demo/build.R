018aec74b76c588de062ee3046fce3fdca815e38
Filippo Gambarota <filippo.gambarota@gmail.com>
2024-01-05 16:49:43 +0100

    updating shiny packages and NA handling

--- inst/shiny-demo/build.R
+++ inst/shiny-demo/build.R
@@ -1,21 +1,3 @@
-# This script is meant just for internal use. The script
-# create a copy of the R/ folder into inst/shiny-demo/ folder
-# to use the functions.
-# the app within inst/shiny-demo/ is a simplified version of the
-# full shiny app inst/shiny. New functions will be used also for 
-# this app but the source code is independent.
-
-# move R/* files into the R folder of the shiny app
-
-app_dir <- "inst/shiny-demo"
-funs <- list.files("R", pattern = "*.R", full.names = TRUE)
-
-if(!dir.exists(file.path(app_dir, "R"))){
-    dir.create(file.path(app_dir, "R"))
-}
-
-file.copy(funs, file.path(app_dir, "R"), overwrite = TRUE)
-
-rsconnect::deployApp("inst/shiny-demo", 
-                     appName = "clustersimulation-demo",
+rsconnect::deployApp("inst/shiny-demo/", 
+                     appName = "clustersimulation-demo", 
                      account = "psicostat")
